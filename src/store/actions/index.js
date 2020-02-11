2241908401a47e675ab926a5bcdf4dae6d1efc08
sidzha <sidzha@ya.ru>
2020-02-11 12:52:17 +0300

    some_edits

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,11 +1,13 @@
 import { DATA_LOADED } from '../constants/action-types';
 
-export const fetchPhotos = () => {
+export const fetchPhotos = (unsplash, toJson) => {
   return function(dispatch) {
+    console.log(unsplash); //undefined
     unsplash.photos
       .listPhotos(1, 11, 'latest')
       .then(toJson)
       .then(json => {
+        console.log(json);
         dispatch({ type: DATA_LOADED, payload: json });
       });
   };
